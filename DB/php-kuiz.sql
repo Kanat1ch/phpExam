68970b3805b48ac47910c8bbdef7d43a2aa18121
Kanat1ch <ilyakanatnikov@mail.ru>
2021-01-28 17:23:41 +0300

    bd dumb

--- DB/php-kuiz.sql
+++ DB/php-kuiz.sql
@@ -1,8 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost
+-- Время создания: Янв 28 2021 г., 14:22
+-- Версия сервера: 5.7.24
+-- Версия PHP: 7.4.1
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `php-kuiz`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `admin`
+--
+
+CREATE TABLE `admin` (
+  `adm_id` int(11) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `admin`
+--
+
+INSERT INTO `admin` (`adm_id`, `password`) VALUES
+(1, '827ccb0eea8a706c4c34a16891f84e7b');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `generator`
+--
+
+CREATE TABLE `generator` (
+  `token` char(40) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `questions`
+--
 
 CREATE TABLE `questions` (
   `qid` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `qno` int(11) NOT NULL,
   `question` text NOT NULL,
   `ans1` text NOT NULL,
@@ -12,54 +68,113 @@ CREATE TABLE `questions` (
   `correct_answer` varchar(1) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Дамп данных таблицы `questions`
+--
 
-INSERT INTO `questions` (`qid`, `qno`, `question`, `ans1`, `ans2`, `ans3`, `ans4`, `correct_answer`) VALUES
-(9, 5, 'What is a correct way to add a comment in PHP?', '&lt;!--&hellip;--&gt;', '/*&hellip;*/', '*\\..\\*', '&lt;comment&gt;&hellip;&lt;/comment&gt;', 'b'),
-(8, 3, 'The PHP syntax is most similar to:', 'Perl and C', 'VBscript', 'Javascript', 'none of these', 'a'),
-(7, 2, 'How do you write "Hello World" in PHP?', 'echo "Hello World";', 'Document.Write("Hello World");', '"Hello World";', 'none of these', 'a'),
-(6, 1, 'What does PHP stand for?', 'Personal Hypertext Processor\r\n', 'Private Home Page', 'Personal Home Page', 'PHP: Hypertext Preprocessor', 'd'),
-(5, 4, 'How do you get information from a form that is submitted using the &quot;get&quot; method?', '$_GET[];', 'Request.Form;', 'Request.QueryString;', 'none of these', 'a'),
-(10, 6, 'When using the POST method, variables are displayed in the URL:', 'True', 'False', 'Can\'t say', 'none of these', 'b'),
-(11, 7, ' Which of the following function is used to get the size of a file?', 'fopen()', 'fread()', 'fsize()', 'filesize()', 'd'),
-(12, 8, 'Which of the following is used to delete a cookie?', 'setcookie()', '$_COOKIE variable', 'isset() function', 'none of the above', 'a');
+INSERT INTO `questions` (`qid`, `id`, `qno`, `question`, `ans1`, `ans2`, `ans3`, `ans4`, `correct_answer`) VALUES
+(16, 1, 3, '12', '123', '12', '1', '3', 'a'),
+(15, 0, 2, 'How do u feel?', 'Good', 'So-so', 'Bad', 'I dont know', 'a'),
+(14, 0, 1, 'What is my name?', 'Ilya', 'Nikita', 'Andrey', 'Dont know', 'a');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Структура таблицы `quiz`
+--
+
+CREATE TABLE `quiz` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `quiz`
+--
+
+INSERT INTO `quiz` (`id`, `title`, `status`) VALUES
+(1, 'Опрос 1', NULL),
+(2, 'Опрос 2', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `email` varchar(225) NOT NULL,
   `played_on` varchar(225) NOT NULL,
-  `score` int(11) NOT NULL DEFAULT '0'
+  `score` int(11) NOT NULL DEFAULT '0',
+  `ip` varchar(255) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Дамп данных таблицы `users`
+--
 
-INSERT INTO `users` (`id`, `email`, `played_on`, `score`) VALUES
-(68, 'admin@gmail.com', '2017-07-31 05:01:43', 0),
-(77, 'root@gmail.com', '2017-07-31 06:52:09', 1),
-(72, 'local@gmail.com', '2017-07-31 06:01:27', 3),
-(80, 'hello@gmail.com', '2017-07-31 06:58:18', 5);
+INSERT INTO `users` (`id`, `email`, `played_on`, `score`, `ip`) VALUES
+(87, 'sdkmf@gmail.com', '2021-01-28 14:03:38', 0, '127.0.0.1');
 
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`adm_id`);
+
+--
+-- Индексы таблицы `questions`
+--
 ALTER TABLE `questions`
   ADD PRIMARY KEY (`qid`);
 
+--
+-- Индексы таблицы `quiz`
+--
+ALTER TABLE `quiz`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
-
--- AUTO_INCREMENT for dumped tables
-
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
 
 --
--- AUTO_INCREMENT for table `questions`
+-- AUTO_INCREMENT для таблицы `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `adm_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `qid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `qid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT для таблицы `quiz`
+--
+ALTER TABLE `quiz`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=88;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
